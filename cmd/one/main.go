26b3a0241777a3d0ac36a9150f83933793c14db7
theeddieh <theeddieharrison@gmail.com>
2017-08-17 12:17:47 -0700

    Add test program.

---
+++ cmd/one/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+    "fmt"
+
+    "github.com/harrisonpizza/main/util"
+)
+
+func main() {
+    fmt.Println(util.One())
+}
---
+++ util/util.go
@@ -0,0 +1,5 @@
+package util
+
+func One() string {
+    return "one"
+}
